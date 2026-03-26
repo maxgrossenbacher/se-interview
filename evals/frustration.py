56b4dcc3ebf5527551c5153c7d9725ebe5b9137c
Max Grossenbacher <grossenbacher.max@gmail.com>
2026-03-26 10:44:51 -0600

    [UPDATE]: add the USER_FRUSTRATION_PROMPT_TEMPLATE text

--- evals/frustration.py
+++ evals/frustration.py
@@ -6,6 +6,26 @@ from phoenix.evals import (
     USER_FRUSTRATION_PROMPT_TEMPLATE,
 )
 
+# USER_FRUSTRATION_PROMPT_TEMPLATE = '''
+# You are given a conversation where between a user and an assistant.
+#   Here is the conversation:
+#   [BEGIN DATA]
+#   *****************
+#   Conversation:
+#   {conversation}
+#   *****************
+#   [END DATA]
+
+#   Examine the conversation and determine whether or not the user got frustrated from the experience.
+#   Frustration can range from midly frustrated to extremely frustrated. If the user seemed frustrated
+#   at the beginning of the conversation but seemed satisfied at the end, they should not be deemed
+#   as frustrated. Focus on how the user left the conversation.
+
+#   Your response must be a single word, either "frustrated" or "ok", and should not
+#   contain any text or characters aside from that word. "frustrated" means the user was left
+#   frustrated as a result of the conversation. "ok" means that the user did not get frustrated
+#   from the conversation.
+# '''
 
 def extract_message(raw):
     """Extract the last message content from a raw span attribute value."""
